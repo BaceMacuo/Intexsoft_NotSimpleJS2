e28baae504df69b79afe96039b76bef1b95e42a2
SkersI <bookao_o@mail.ru>
2021-07-24 23:13:50 +0300

    add function SortArr.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,36 @@
-console.log('hello world');
+const arr = [0, 25, -1, 99, -9, 2];
+let a = null;
+function Sort (arr) {
+    let choice = prompt(' Имеющийся массив ' + arr + '\n В каком порядке сортируем: \n 1 - Возрастание; \n 2 - Убывание. ', '');
+    switch(choice){
+        case '1':
+            for(let i = 1; i < arr.length; i++){
+                for(let j = arr.length; j >= 0; j --){
+                    if(arr[j+1]<=arr[j]){
+                        a = arr[j];
+                        arr[j] = arr[j+1];
+                        arr[j+1] = a;
+                        console.log(arr);
+                    }
+                }        
+            }
+            alert(arr);
+        break;
+        case '2':
+            for(let i = 1; i < arr.length; i++){
+                for(let j = arr.length; j >= 0; j --){
+                    if(arr[j+1]>=arr[j]){
+                        a = arr[j];
+                        arr[j] = arr[j+1];
+                        arr[j+1] = a;
+                        console.log(arr);
+                    }
+                }        
+            }
+            alert(arr);
+        break;
+        default: alert('Перечитай задание!');
+        Sort(arr);
+    }    
+}
+Sort(arr);
